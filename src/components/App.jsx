d0f9c62ed9367c761b49f462a02303ee5959aba5
MAYURDG <mayurgoradiya8322@gmail.com>
2024-07-02 22:57:42 +0530

    second commit adding functionality to keeper app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,39 @@
-import React from "react"
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
-import notes from "../notes"
-
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return (
-      <div>
-        <Header />
-        {notes.map((noteitem) => <Note key={noteitem.key} title={noteitem.title} content={noteitem.content}/> )}
-        <Footer />
-      </div>
-    )
+
+  const [notes, setNotes] = useState([])
+
+  function addNote(note) {
+    setNotes(prevNotes => {
+      return [...prevNotes, note]
+    })
   }
+
+  function deleteNote(id) {
+   setNotes(prevNotes => {
+     return prevNotes.filter((noteItem, index) => {
+       return index!== id;
+     })
+   })
+}
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote}/>
+      {
+        notes.map((note, index) => {
+          return <Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote} />
+        })
+      }
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
